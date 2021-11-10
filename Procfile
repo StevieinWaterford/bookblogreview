15cb956c864582271c8e2925bc4e321d31e94448
StevieinWaterford <stevieindub@gmail.com>
2021-11-10 23:17:22 +0000

    Add deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn book-reviews-blog.wsgi
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://exzdnmdifofpfr:bb3a5adfa3f4d1cfa5c49c8853833bac4da3d1983d1a6d20c11674982742da15@ec2-54-76-249-45.eu-west-1.compute.amazonaws.com:5432/d7k97l078plqk5"
+os.environ["SECRET_KEY"] = "alanSecretKey2021$$"
+os.environ["CLOUDINARY_URL"] = "cloudinary://884173494267683:2A1aK79n30uxfpSjxtgcjAXpt_g@dzbad1rgb"
